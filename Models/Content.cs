23b86c1fc2eee3eb7f1824894c545d9d5d6995a0
akadirgunduz <gunduzabdulkadir@gmail.com>
2023-01-11 09:25:49 +0300

    11.01.2023

---
+++ Models/Content.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace GibiSu.Models
+{
+    public class Content
+    {
+        public long Id { get; set; }
+
+        [Column(TypeName = "nchar(50)")]
+        [Required(ErrorMessage = "Bu alan gerekli")]
+        [DisplayName("Başlık")]
+        [StringLength(50, ErrorMessage = "En fazla 50 karakter")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "ntext")]
+        [Required(ErrorMessage = "Bu alan gerekli")]
+        [DisplayName("İçerik")]
+        public string Text { get; set; }
+
+        [DisplayName("Görsel")]
+        [NotMapped]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public IFormFile FormImage { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [Column(TypeName = "image")]
+        public byte[] Image { get; set; }
+    }
+}
---
+++ Models/Page.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace GibiSu.Models
+{
+    public class Page
+    {
+        [Key] 
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required(ErrorMessage = "Bu alan gerekli")]
+        [DisplayName("Url")]
+        [StringLength(100, ErrorMessage = "En fazla 100 karakter")]
+        public string Url { get; set; }
+
+        [DisplayName("Banner")]
+        [NotMapped]
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public IFormFile FormImage { get; set; }
+
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [Column(TypeName = "image")]
+        public byte[] Banner { get; set; }
+
+
+
+        [DisplayName("Menü (opsiyonel)")]
+        public short? MenuId { get; set; }
+
+        [DisplayName("Menü")]
+        [ForeignKey("MenuId")]
+        public Menu? Menu { get; set; }
+
+    }
+}
