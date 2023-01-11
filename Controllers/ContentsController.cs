1a0b3fe3bf28ab0f47cce834c39d1db841699493
akadirgunduz <gunduzabdulkadir@gmail.com>
2023-01-11 12:50:37 +0300

    Controller-11.01

---
+++ Controllers/ContentsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GibiSu.Data;
+using GibiSu.Models;
+
+namespace GibiSu.Controllers
+{
+    public class ContentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contents
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Contents.Include(c => c.Page);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Contents/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Contents == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .Include(c => c.Page)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // GET: Contents/Create
+        public IActionResult Create()
+        {
+            ViewData["PageUrl"] = new SelectList(_context.Pages, "Url", "Url");
+            return View();
+        }
+
+        // POST: Contents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Text,Image,Order,PageUrl")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(content);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PageUrl"] = new SelectList(_context.Pages, "Url", "Url", content.PageUrl);
+            return View(content);
+        }
+
+        // GET: Contents/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Contents == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents.FindAsync(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+            ViewData["PageUrl"] = new SelectList(_context.Pages, "Url", "Url", content.PageUrl);
+            return View(content);
+        }
+
+        // POST: Contents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Title,Text,Image,Order,PageUrl")] Content content)
+        {
+            if (id != content.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(content);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContentExists(content.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PageUrl"] = new SelectList(_context.Pages, "Url", "Url", content.PageUrl);
+            return View(content);
+        }
+
+        // GET: Contents/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Contents == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Contents
+                .Include(c => c.Page)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // POST: Contents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Contents == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Contents'  is null.");
+            }
+            var content = await _context.Contents.FindAsync(id);
+            if (content != null)
+            {
+                _context.Contents.Remove(content);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContentExists(long id)
+        {
+          return _context.Contents.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/PagesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GibiSu.Data;
+using GibiSu.Models;
+
+namespace GibiSu.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pages
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pages.Include(p => p.Menu);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Pages/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Pages
+                .Include(p => p.Menu)
+                .FirstOrDefaultAsync(m => m.Url == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // GET: Pages/Create
+        public IActionResult Create()
+        {
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Name");
+            return View();
+        }
+
+        // POST: Pages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Url,Banner,MenuId")] Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Name", page.MenuId);
+            return View(page);
+        }
+
+        // GET: Pages/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Pages.FindAsync(id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Name", page.MenuId);
+            return View(page);
+        }
+
+        // POST: Pages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Url,Banner,MenuId")] Page page)
+        {
+            if (id != page.Url)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(page);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PageExists(page.Url))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Name", page.MenuId);
+            return View(page);
+        }
+
+        // GET: Pages/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Pages
+                .Include(p => p.Menu)
+                .FirstOrDefaultAsync(m => m.Url == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // POST: Pages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Pages == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Pages'  is null.");
+            }
+            var page = await _context.Pages.FindAsync(id);
+            if (page != null)
+            {
+                _context.Pages.Remove(page);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PageExists(string id)
+        {
+          return _context.Pages.Any(e => e.Url == id);
+        }
+    }
+}
