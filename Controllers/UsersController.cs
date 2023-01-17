55990e5fa72c6e75e969f8c7663f4fc42484b669
Kadikoybilisim <bilisimegitimkadikoy34@gmail.com>
2023-01-17 14:10:36 +0300

    login

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,73 @@
+﻿using GibiSu.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace GibiSu.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        SignInManager<ApplicationUser> _signInManager;
+
+        public UsersController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_signInManager.UserManager.Users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,UserName,Password,Email,ConfirmPassword,Agreed,Address")] ApplicationUser applicationUser)
+        {
+            ModelState.Remove("Orders");
+
+            if (ModelState.IsValid)
+            {
+                _signInManager.UserManager.CreateAsync(applicationUser, applicationUser.Password).Wait();
+                return Redirect("~/");
+            }
+
+            return View(applicationUser);
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string userName, string password)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult identityResult;
+
+            if (ModelState.IsValid)
+            {
+                identityResult = _signInManager.PasswordSignInAsync(userName, password, false, false).Result;
+
+                if (identityResult.Succeeded == true)
+                {
+                    return Redirect("~/");
+                }
+
+            }
+
+            return View();
+
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using GibiSu.Data;
+using GibiSu.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,7 +18,7 @@ namespace GibiSu
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
